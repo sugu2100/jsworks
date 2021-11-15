b881a135af106790722db37437ffd5adfa7d1698
sugu2100 <sugu2100@daum.net>
2021-11-15 12:55:04 +0900

    pdf 추가

--- day06/js/calc_score.js
+++ day06/js/calc_score.js
@@ -7,12 +7,19 @@ var score = [
     [100, 90],
     [85, 70]
 ]
-var korSum = 0;
-var mathSum = 0;
+var sum = [0, 0];
+var avg = [0.0, 0.0];
 
 //점수 합계
 for(var i = 0; i < score.length; i++){
-    korSum += score[i][0];
+    sum[0] += score[i][0];   //1열, 국어 합계
+    sum[1] += score[i][1];  //2열,  수학 합계
 }
+avg[0] = sum[0] / score.length;  //국어 평균
+avg[1] = sum[1] / score.length;  //수학 평균
 
-document.write(korSum);
+//출력
+document.write("국어 총점 : " + sum[0] + "<br>");
+document.write("수학 총점 : " + sum[1] + "<br>");
+document.write("국어 평균 : " + avg[0] + "<br>");
+document.write("수학 평균 : " + avg[1] + "<br>");
